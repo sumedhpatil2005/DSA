95756b8c95798caa26d710eeb9f211205acf756d
sumedh <sumedhpatil2005@gmail.com>
2026-01-21 14:52:58 +0530

    temp

---
+++ aggcow.cpp
@@ -0,0 +1,60 @@
+
+#include<iostream>
+using namespace std;
+#include<vector>
+#include <algorithm>
+bool isPossible(vector<int> &stalls, int k, int mid, int n) {
+    
+    int cowCount = 1;
+    int lastPos = stalls[0];
+    
+    for(int i=0; i<n; i++ ){
+        
+        if(stalls[i]-lastPos >= mid){
+            cowCount++;
+            if(cowCount==k)
+            {
+                return true;
+            }
+            lastPos = stalls[i];
+        }
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(), stalls.end());
+   	int s = 0;
+    int n = stalls.size();
+    int e=stalls[n-1];
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    
+    while(s<=e) {
+        if(isPossible(stalls, k, mid, n)) {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+int main() {
+    int n, k;
+    cin >> n >> k;
+    
+    vector<int> stalls(n);
+    for(int i = 0; i < n; i++) {
+        cin >> stalls[i];
+    }
+    
+    int result = aggressiveCows(stalls, k);
+    cout << result << endl;
+    
+    return 0;
+}
