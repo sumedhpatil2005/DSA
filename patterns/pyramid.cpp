5a9fd25b180ba3a01ce868ee140bdd219b629e75
sumedh <sumedhpatil2005@gmail.com>
2026-04-19 21:21:18 +0530

    f

---
+++ patterns/pyramid.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int pyramid1(int n){
+           
+}
+
+int main(){
+    pyramid1(4);
+} 
---
+++ trianglepatterrn.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int triangle(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+}
+
+
+int main(){
+    triangle(4);
+} 
