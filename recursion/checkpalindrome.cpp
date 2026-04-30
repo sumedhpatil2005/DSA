3d4113ee914bfad0490a24d4da957c28a5fb277f
sumedh <sumedhpatil2005@gmail.com>
2026-04-30 09:45:21 +0530

    i

---
+++ recursion/checkpalindrome.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+bool f(int i,string s){
+    if(i>s.length()/2)return true;
+    if(s[i]!=s[s.length()-i-1])return false;
+    return f(i+1,s);
+}
+
+int main(){
+    string s ="madam";
+    cout<<f(0,s);
+}
